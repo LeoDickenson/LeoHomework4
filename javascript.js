5511d995e8b15bbdfea2c90f82f54490aa33e381
Leo Dickens <dickens.leo@gmail.com>
2021-12-21 23:47:37 -0600

    set up funcions, syntax errors

--- javascript.js
+++ javascript.js
@@ -2,19 +2,59 @@
 var quizDiv = document.getElementById('quiz');
 var resultsDiv = document.getElementById('results');
 var submitButton = document.getElementById('submit');
-var ogButton = document.getElementById('ogBtn')
+// var ogButton = document.getElementById('ogBtn');
 
 var quizQs = [
     {
         question:'?',
-        answers:{},
+        answers:{
+            a: 'A',
+            b: 'A',
+            c: 'A',
+        },
         winner: 'c'
-    }
+    },
+    {
+        question:'?',
+        answers:{
+            a: 'A',
+            b: 'A',
+            c: 'A',
+        },
+        winner: 'c'
+    },
 ]
 
 function startGame() {}
 
-function buildQuiz(){}
+function runQuiz(){
+    
+    var output = [];    
+    var answersrecall = 
+    [' ',
+        ' '
+    ];
+
+    quizQs.forEach((currentQ, qNumber) >=
+    {
+
+        for(currentQ: answers) {
+
+            answers.push(
+                `<label>
+                <input type="radio" name="question${qNumber}" value="${letter}">
+                ${letter} :
+                ${currentQuestion.answersrecall[letter]}
+                </label>`
+            );
+        }
+            output.push(
+                `<div class="question"> ${currentQ.question} </div>
+                <div class="answers"> ${answersrecall.join('')} </div>`
+            );
+        }
+    );
+}
 
 function showResults(){}
 
--- readme.md
+++ readme.md
@@ -1,1 +1,2 @@
 added some basic html and css
+struggling with syntax errors after setting up functions, trying to resolve
