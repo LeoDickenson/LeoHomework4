4575ebe4fa2c21c7e840fadd5949aeac0e28623d
Leo Dickens <dickens.leo@gmail.com>
2021-12-21 22:19:33 -0600

    added functions

--- javascript.js
+++ javascript.js
@@ -1,1 +1,26 @@
-// have get 
+//
+var quizDiv = document.getElementById('quiz');
+var resultsDiv = document.getElementById('results');
+var submitButton = document.getElementById('submit');
+var ogButton = document.getElementById('ogBtn')
+
+var quizQs = [
+    {
+        question:'?',
+        answers:{},
+        winner: 'c'
+    }
+]
+
+function startGame() {}
+
+function buildQuiz(){}
+
+function showResults(){}
+
+// display quiz on clicking ogBtn
+runQuiz();
+
+// on submit, show results
+submitButton.addEventListener('click', showResults);
+ogButton.addEventListener('click', beginGame);
